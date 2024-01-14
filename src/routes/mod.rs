5fe60f3e0c16920c669dc6f7faf0e8f50a4534bb
cutymeoRBLX <hkhanhnam81@gmail.com>
2024-01-14 19:41:17 +0700

    Implement whitelist and id system routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,15 +1,116 @@
-use std::borrow::Borrow;
-
+use poem::Result;
+use poem_openapi::{param::Query, param::Header, payload::Json, payload::PlainText, OpenApi};
 use liquid_breakout_backend::Backend;
+use structs::{IdResponse, WhitelistInfo, WhitelistResponse};
 
-pub mod structs;
-mod whitelist;
+mod structs;
 
 pub struct Routes {
     backend: Backend
 }
+
+#[OpenApi]
 impl Routes {
     pub fn new(backend: Backend) -> Self {
         Self { backend: backend }
+    }
+
+    pub async fn authorized(&self, api_key: Header<Option<String>>) -> bool {
+        let api_key = match api_key.0 {
+            Some(k) => k,
+            None => return false,
+        };
+        let valid = self.backend.is_valid_api_key(api_key.as_str()).await;
+        match valid {
+            Ok(valid) => {
+                return valid
+            },
+            Err(_) => return false,
+        };
+    }
+
+    // Map Test Whitelist
+    #[oai(path = "/maptest/whitelist", method = "post")]
+    pub async fn whitelist(&self, asset_id: Query<Option<u64>>) -> Result<WhitelistResponse> {
+        let asset_id = match asset_id.0 {
+            None => return Ok(WhitelistResponse::BadRequest(
+                Json(
+                    WhitelistInfo {
+                        success: false,
+                        error: Some("Invalid Asset ID.".to_string()),
+                        share_id: None   
+                    }
+                )
+            )),
+            Some(b) => b,
+        };
+
+        let result = self.backend.whitelist_asset(asset_id, 0).await;
+        match result {
+            Ok(_) => Ok(WhitelistResponse::Ok(
+                Json(
+                    WhitelistInfo {
+                        success: true,
+                        error: None,
+                        share_id: Some(self.backend.get_shareable_id(asset_id.to_string()).unwrap())
+                    }
+                )
+            )),
+            Err(e) => Ok(WhitelistResponse::ServerError(Json(
+                WhitelistInfo {
+                    success: false,
+                    error: Some((*e).to_string()),
+                    share_id: None   
+                }
+            )))
+        }
+    }
+
+    // Map Test ID System
+    #[oai(path = "/maptest/id/share", method = "get")]
+    pub async fn get_shareable_id(&self, id: Query<Option<u64>>) -> Result<IdResponse> {
+        let id = match id.0 {
+            None => return Ok(IdResponse::InvalidId),
+            Some(b) => b,
+        };
+        let result = self.backend.get_shareable_id(id.to_string());
+        match result {
+            Ok(share_id) => Ok(IdResponse::Ok(PlainText(share_id))),
+            Err(e) => Ok(IdResponse::ServerError(PlainText((*e).to_string())))
+        }
+    }
+
+    #[oai(path = "/maptest/id/number", method = "get")]
+    pub async fn get_number_id(&self, api_key: Header<Option<String>>, id: Query<Option<u64>>) -> Result<IdResponse> {
+        let authorized = self.authorized(api_key).await;
+        if !authorized {
+            return Ok(IdResponse::Unauthorized)
+        }
+
+        let id = match id.0 {
+            Some(i) => i,
+            None => return Ok(IdResponse::InvalidId),
+        };
+        let result = self.backend.get_shareable_id(id.to_string());
+        match result {
+            Ok(share_id) => Ok(IdResponse::Ok(PlainText(share_id))),
+            Err(e) => Ok(IdResponse::ServerError(PlainText((*e).to_string())))
+        }
+    }
+
+    // Map Test Map Hub System
+    #[oai(path = "/maptest/hub/fetch", method = "get")]
+    pub async fn fetch_maphub_data(&self) {
+
+    }
+
+    #[oai(path = "/maptest/hub/publish", method = "post")]
+    pub async fn publish_to_maphub(&self) {
+
+    }
+
+    #[oai(path = "/maptest/hub/validatehash", method = "post")]
+    pub async fn validate_hash_with_maphub(&self) {
+
     }
 }
--- src/routes/structs.rs
+++ src/routes/structs.rs
@@ -1,1 +1,37 @@
-pub struct BadResponse {}
+use poem_openapi::{payload::Json, payload::PlainText, ApiResponse, Object};
+
+// Map Test's Whitelist route
+#[derive(Object)]
+pub struct WhitelistInfo {
+    pub success: bool,
+    pub error: Option<String>,
+    pub share_id: Option<String>
+}
+
+#[derive(ApiResponse)]
+pub enum WhitelistResponse {
+    #[oai(status = 200)]
+    Ok(Json<WhitelistInfo>),
+
+    #[oai(status = 400)]
+    BadRequest(Json<WhitelistInfo>),
+
+    #[oai(status = 500)]
+    ServerError(Json<WhitelistInfo>)
+}
+
+// Map Test's ID system routes
+#[derive(ApiResponse)]
+pub enum IdResponse {
+    #[oai(status = 200)]
+    Ok(PlainText<String>),
+
+    #[oai(status = 400)]
+    InvalidId,
+
+    #[oai(status = 401)]
+    Unauthorized,
+
+    #[oai(status = 500)]
+    ServerError(PlainText<String>)
+}
