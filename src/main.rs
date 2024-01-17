aa44404d9bc994d0567b9843b8c9e822e7cb3312
cutymeoRBLX <hkhanhnam81@gmail.com>
2024-01-17 17:50:40 +0700

    bootstrap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use poem::{endpoint::StaticFilesEndpoint, listener::TcpListener, Route};
 use poem_openapi::OpenApiService;
 use routes::Routes;
@@ -8,7 +9,25 @@ mod routes;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let backend = Backend::new("".to_string(), vec!["".to_string(), "".to_string()]);
+    // Check for environment variables
+    let roblox_cookie = env::var("ROBLOX_COOKIE").expect("Server cannot start: Failed to read ROBLOX_COOKIE from environment");
+    let mongodb_url = env::var("MONGODB_URL").expect("Server cannot start: Failed to read MONGODB_URL from environment");
+
+    println!("Server starting up.");
+
+    let mut backend = Backend::new(
+        roblox_cookie,
+        vec![
+            "123456789*=+-aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ".to_string(),
+            "0123456789".to_string()
+        ]
+    );
+    let connect_result = backend.connect_mongodb(mongodb_url).await;
+    match connect_result {
+        Ok(_) => {},
+        Err(e) => panic!("Server cannot start: Failed to connect to MongoDB, reason: {}", (*e).to_string())
+    }
+
     let routes = Routes::new(backend);
 
     let api_service = OpenApiService::new(routes, "Liquid Breakout API", "0.0.1")
@@ -19,6 +38,8 @@ async fn main() -> Result<(), std::io::Error> {
         .nest("/", StaticFilesEndpoint::new("./assets").index_file("index.html"))
         .nest("/api", api_service)
         .nest("/api/docs", api_swagger);
+
+    println!("Starting Server, listening at port: 3000");
 
     poem::Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(server_routes)
